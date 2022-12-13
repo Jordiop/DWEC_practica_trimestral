37f159e01434b632e89521b2a447582924db743e
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2022-12-14 00:06:01 +0100

    Get one and get all

--- client/script.js
+++ client/script.js
@@ -1,19 +1,50 @@
 const url = 'http://localhost:3000/heroe';
 
-function prueba() {
-    // Get all the heroes from the server and print them to the html
+// Get all
+function verHeroes() {
     fetch(url)
         .then(res => res.json())
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                document.write('<div>' +
-                    data[i].id + " " + data[i].nombre + ' ' + data[i].avenger
-
-                    +
-                    '</div>')
+        .then(avenger => {
+            const container = document.getElementById('container');
+            for (let i = 0; i < avenger.length; i++) {
+                let tarjetas = document.createElement('div');
+                tarjetas.classList.add('tarjeta');
+                tarjetas.innerHTML =
+                    '<img src="../images/heroe' + i + '.jpeg' + '" alt="imagen" width="100">' +
+                    '<div>' + 'ID: ' + avenger[i].id + '</div>' +
+                    '<div>' + 'Nombre: ' + avenger[i].nombre + '</div>' +
+                    '<div>' + 'Status: ' + avenger[i].avenger + '</div>' +
+                    '<div>' + 'Points: ' + avenger[i].points + '</div>';
+                container.appendChild(tarjetas);
             }
         })
         .catch(err => {
             console.log(err);
         });
 }
+
+// Get one
+function buscarHeroe() {
+    let id = document.getElementById('busqueda').value;
+    fetch(url)
+        .then(res => res.json())
+        .then(avenger => {
+            const container = document.getElementById('resultadoBusqueda');
+            for (let i = 0; i < avenger.length; i++) {
+                if (id == avenger[i].id) {
+                    let tarjetas = document.createElement('div');
+                    tarjetas.classList.add('tarjeta');
+                    tarjetas.innerHTML =
+                        '<img src="../images/heroe' + i + '.jpeg' + '" alt="imagen" width="100">' +
+                        '<div>' + 'ID: ' + avenger[i].id + '</div>' +
+                        '<div>' + 'Nombre: ' + avenger[i].nombre + '</div>' +
+                        '<div>' + 'Status: ' + avenger[i].avenger + '</div>' +
+                        '<div>' + 'Points: ' + avenger[i].points + '</div>';
+                    container.appendChild(tarjetas);
+                }
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
