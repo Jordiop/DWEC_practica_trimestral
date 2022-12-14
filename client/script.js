b3eeb0877e35cdad1ea81eff1a08a51f31e679f7
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2022-12-14 11:39:10 +0100

    Post

--- client/script.js
+++ client/script.js
@@ -32,6 +32,7 @@ async function buscarHeroe() {
             let id = document.getElementById('busqueda').value;
             let avenger = await response.json();
             const container = document.getElementById('resultadoBusqueda');
+            // Emptying the container to have just one result
             container.innerHTML = '';
             for (let i = 0; i < avenger.length; i++) {
                 if (id == avenger[i].id) {
@@ -53,36 +54,38 @@ async function buscarHeroe() {
 };
 
 
-// Post
+// Post async-await version
 async function agregarHeroe() {
     try {
-        let nombre = document.getElementById('nombreAgregar').value;
-        let imagen = document.getElementById('imagenAgregar').value;
-        let vida = document.getElementById('vidaAgregar').value;
-        let status = document.getElementById('statusAgregar').value;
-        // Comprobamos que los campos no esten vacios
-        if (nombre != '' || imagen != '' || vida != '' || status != '') {
-            fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        nombre: nombre,
-                        imagen: imagen,
-                        vidas: vida,
-                        status: status
+        let response = await fetch(url);
+        if (response.status === 200) {
+            let nombre = document.getElementById('nombreAgregar').value;
+            let imagen = document.getElementById('imagenAgregar').value;
+            let vida = document.getElementById('vidaAgregar').value;
+            let status = document.getElementById('statusAgregar').value;
+            // Comprobamos que los campos no esten vacios
+            if (nombre != '' || imagen != '' || vida != '' || status != '') {
+                fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            nombre: nombre,
+                            imagen: imagen,
+                            vidas: vida,
+                            status: status
+                        })
                     })
-                })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }
         }
-
     } catch (err) {
         console.log(err);
     }
