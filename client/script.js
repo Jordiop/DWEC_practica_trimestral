2335e62fb5e19ab5f2f87294da61faf5aaf13a10
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2022-12-14 01:21:22 +0100

    Post empezado

--- client/script.js
+++ client/script.js
@@ -14,7 +14,7 @@ function verHeroes() {
                     '<div>' + 'ID: ' + avenger[i].id + '</div>' +
                     '<div>' + 'Nombre: ' + avenger[i].nombre + '</div>' +
                     '<div>' + 'Status: ' + avenger[i].avenger + '</div>' +
-                    '<div>' + 'Points: ' + avenger[i].points + '</div>';
+                    '<div>' + 'Vida: ' + avenger[i].vida + '</div>';
                 container.appendChild(tarjetas);
             }
         })
@@ -32,6 +32,7 @@ function buscarHeroe() {
             const container = document.getElementById('resultadoBusqueda');
             for (let i = 0; i < avenger.length; i++) {
                 if (id == avenger[i].id) {
+                    ''
                     let tarjetas = document.createElement('div');
                     tarjetas.classList.add('tarjeta');
                     tarjetas.innerHTML =
@@ -39,7 +40,7 @@ function buscarHeroe() {
                         '<div>' + 'ID: ' + avenger[i].id + '</div>' +
                         '<div>' + 'Nombre: ' + avenger[i].nombre + '</div>' +
                         '<div>' + 'Status: ' + avenger[i].avenger + '</div>' +
-                        '<div>' + 'Points: ' + avenger[i].points + '</div>';
+                        '<div>' + 'Vida: ' + avenger[i].vida + '</div>';
                     container.appendChild(tarjetas);
                 }
             }
@@ -48,3 +49,38 @@ function buscarHeroe() {
             console.log(err);
         });
 };
+
+// Post
+const agregarHeroe = async() => {
+    try {
+        let nombre = document.getElementById('nombreAgregar').value;
+        let imagen = document.getElementById('imagenAgregar').value;
+        let vida = document.getElementById('vidaAgregar').value;
+        let status = document.getElementById('statusAgregar').value;
+        // Comprobamos que los campos no esten vacios
+        if (nombre != '' || imagen != '' || vida != '' || status != '') {
+            fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        nombre: nombre,
+                        imagen: imagen,
+                        vidas: vida,
+                        status: status
+                    })
+                })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
