3d18e2916b66a878d944ea523c43f67eaaadaeb9
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2022-12-14 10:54:41 +0100

    Async update

--- client/script.js
+++ client/script.js
@@ -1,12 +1,37 @@
 const url = 'http://localhost:3000/heroe';
 
-// Get all
-function verHeroes() {
-    fetch(url)
-        .then(res => res.json())
-        .then(avenger => {
-            const container = document.getElementById('container');
-            for (let i = 0; i < avenger.length; i++) {
+// Get all async version
+async function verHeroes() {
+    try {
+        let response = await fetch(url);
+        let avenger = await response.json();
+        const container = document.getElementById('container');
+        for (let i = 0; i < avenger.length; i++) {
+            let tarjetas = document.createElement('div');
+            tarjetas.classList.add('tarjeta');
+            tarjetas.innerHTML =
+                '<img src="../images/heroe' + i + '.jpeg' + '" alt="imagen" width="100">' +
+                '<div>' + 'ID: ' + avenger[i].id + '</div>' +
+                '<div>' + 'Nombre: ' + avenger[i].nombre + '</div>' +
+                '<div>' + 'Status: ' + avenger[i].avenger + '</div>' +
+                '<div>' + 'Vida: ' + avenger[i].vida + '</div>';
+            container.appendChild(tarjetas);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Get one
+async function buscarHeroe() {
+    try {
+        let id = document.getElementById('busqueda').value;
+        let response = await fetch(url);
+        let avenger = await response.json();
+        const container = document.getElementById('resultadoBusqueda');
+        for (let i = 0; i < avenger.length; i++) {
+            if (id == avenger[i].id) {
+                ''
                 let tarjetas = document.createElement('div');
                 tarjetas.classList.add('tarjeta');
                 tarjetas.innerHTML =
@@ -17,41 +42,14 @@ function verHeroes() {
                     '<div>' + 'Vida: ' + avenger[i].vida + '</div>';
                 container.appendChild(tarjetas);
             }
-        })
-        .catch(err => {
-            console.log(err);
-        });
-}
-
-// Get one
-function buscarHeroe() {
-    let id = document.getElementById('busqueda').value;
-    fetch(url)
-        .then(res => res.json())
-        .then(avenger => {
-            const container = document.getElementById('resultadoBusqueda');
-            for (let i = 0; i < avenger.length; i++) {
-                if (id == avenger[i].id) {
-                    ''
-                    let tarjetas = document.createElement('div');
-                    tarjetas.classList.add('tarjeta');
-                    tarjetas.innerHTML =
-                        '<img src="../images/heroe' + i + '.jpeg' + '" alt="imagen" width="100">' +
-                        '<div>' + 'ID: ' + avenger[i].id + '</div>' +
-                        '<div>' + 'Nombre: ' + avenger[i].nombre + '</div>' +
-                        '<div>' + 'Status: ' + avenger[i].avenger + '</div>' +
-                        '<div>' + 'Vida: ' + avenger[i].vida + '</div>';
-                    container.appendChild(tarjetas);
-                }
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
+        }
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 // Post
-const agregarHeroe = async() => {
+async function agregarHeroe() {
     try {
         let nombre = document.getElementById('nombreAgregar').value;
         let imagen = document.getElementById('imagenAgregar').value;
